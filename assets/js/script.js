1e912f9c77c3820227f20e8e155569dda9c16135
jorddo <jordanbsneed@gmail.com>
2022-01-23 10:33:26 -0600

    updating draggables to work on mobile

--- assets/js/script.js
+++ assets/js/script.js
@@ -204,3 +204,18 @@ $('#remove-tasks').on('click', function () {
 
 // load tasks for the first time
 loadTasks();
+
+$('#trash').droppable({
+  accept: '.card .list-group-item',
+  tolerance: 'touch',
+  drop: function (event, ui) {
+    console.log('drop');
+    ui.draggable.remove();
+  },
+  over: function (event, ui) {
+    console.log('over');
+  },
+  out: function (event, ui) {
+    console.log('out');
+  },
+});
